9e2873180023293aad4e93fc183c1ad64cc6aa48
nengkhoiba <Nengkhoi@macbook.local>
2018-03-09 21:35:41 +0530

    main page

--- application/views/data/global_header.php
+++ application/views/data/global_header.php
@@ -79,7 +79,7 @@
 		<div class="header-bottom"><!--header-bottom-->
 			<div class="container">
 				<div class="row">
-					<div class="col-sm-9">
+					<div class="col-sm-6">
 						<div class="navbar-header">
 							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
 								<span class="sr-only">Toggle navigation</span>
@@ -111,7 +111,7 @@
 							</ul>
 						</div>
 					</div>
-					<div class="col-sm-3">
+					<div class="col-sm-6">
 						<div class="search_box pull-right">
 							<input type="text" placeholder="Search"/>
 						</div>
