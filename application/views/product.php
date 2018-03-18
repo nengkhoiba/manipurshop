60f933efa2dd090e1815a8d0cd6482ecc375bfdc
nengkhoiba <Nengkhoi@macbook.local>
2018-03-18 18:48:23 +0530

    changes

--- application/views/product.php
+++ application/views/product.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Murolen an E-Commerce platform only for manipur.">
+    <meta name="author" content="">
+    <title>Home | Murolen</title>
 <?php $this->load->view('data/global_header')?>
 	
 	<section>
