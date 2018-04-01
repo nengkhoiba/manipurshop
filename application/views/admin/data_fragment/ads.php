162ff1e51dc2ab307bf271c9cae344aa8a1c70d0
Thoisana2016 <keishamthoisana@gmail.com>
2018-04-01 20:37:32 +0530

    Ads Update

--- application/views/admin/data_fragment/ads.php
+++ application/views/admin/data_fragment/ads.php
@@ -1,10 +1,12 @@
-< 
- <table  id="brand_data" class="table table-striped table-bordered table-hover">
+
+ <table  id="" class="table table-striped table-bordered table-hover">
                                     <thead>
                                         <tr>
                                             <th>ID</th>
                                             <th>Title</th>
                                             <th>Description</th>
+                                            <th>Image</th>
+                                            <th>URL</th>
                                             <th>Status</th>
                                             <th>Edit</th>
                                             <th>Delete</th>
@@ -14,7 +16,7 @@
                                        <?php 
                                        		
                                        		
-                                       		$sql="SELECT `ID`, `Code`, `Description`, `Added_by`, `Added_on`, `Modified_by`, `Modified_on`, `isActive` FROM `Brand` WHERE isActive =1 ";
+                                       		$sql="SELECT `ID`, `Title`, `Description`, `Image`, `url`,`Status` FROM `Advertise` WHERE isActive=1 ";
                                        		$query = $this->db->query($sql);
                                        		if($query){
                                        			while ($result = mysql_fetch_array($query->result_id)){
@@ -22,11 +24,17 @@
                                        				
                                        				 <tr>
 			                                        	<td><?php echo $result['ID']; ?></td>
-			                                        	<td><?php echo $result['Code']; ?></td>
+			                                        	<td><?php echo $result['Title']; ?></td>
 			                                        	<td><?php echo $result['Description']; ?></td>
-			                                        	<td><?php echo $result['Added_on']; ?></td>
-			                                        	<td><i style="cursor: pointer;" class="fa fa-edit" onclick="edit('<?php echo $result['ID'];?>','<?php echo $result['Code'];?>','<?php echo $result['Description'];?>')" ></i></td>
-			                                        	<td><i style="cursor: pointer" onclick="remove('<?php echo $result['ID']; ?>')" class="fa fa-remove"></i></td>
+			                                        	<td><img style="width: 100px" src="<?php echo base_url();?><?php echo $result['Image']; ?>" alt="image"></td>
+			                                        	<td><?php echo $result['url']?></td>
+			                                        	<?php if($result['Status']==0){?>
+			                                        	<td ><a class="label label-danger">Unpublish</a></td>
+			                                        	<?php }else{?>
+			                                        	<td ><a class="label label-success"> Publish</a></td>
+			                                        	<?php }?>
+			                                        	<td><i style="cursor: pointer;" class="fa fa-edit" onclick="editAdsDetails('<?php echo $result['ID'];?>','<?php echo $result['Title'];?>','<?php echo $result['Description'];?>','<?php echo $result['url']?>')" ></i></td>
+			                                        	<td><i style="cursor: pointer" onclick="removeAdsDetails('<?php echo $result['ID']; ?>')" class="fa fa-remove"></i></td>
 			                                        </tr>
                                        				
                                        				<?php
